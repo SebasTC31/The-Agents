7d937682cd5d517b9abb3463478e1063bcf00e04
SebasTC31 <juansebas0464@gmail.com>
2024-04-27 19:13:52 -0500

    Cadena de conexión agregada y nuevo paquete

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,30 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//builder.Services.AddDbContext<DataBaseContext>(o =>
+//{
+//    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//});
+
+//builder.Services.AddTransient<SeederDb>();
+
+//builder.Services.AddControllers().AddJsonOptions(x =>
+//                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 var app = builder.Build();
+
+//Esto me llama el método SeederAsync() para poder poblar las tablas al momento de correr la app.
+//SeederData();
+//void SeederData()
+//{
+//    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+//    using (IServiceScope? scope = scopedFactory.CreateScope())
+//    {
+//        SeederDb? service = scope.ServiceProvider.GetService<SeederDb>();
+//        service.SeederAsync().Wait();
+//    }
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
