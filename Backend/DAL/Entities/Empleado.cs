38aaf04e0b401ffb0d69f3ca4e14fe436ae40d70
DIEGO ALEXANDER VALENCIA CALDERÓN <diegovalencia313667@correo.itm.edu.co>
2024-04-28 19:13:34 -0500

    Revert "ModelBuilder para las tablas de gerente y empleado"
    This reverts commit 21578f7556c36088ba553425a4eb54dfdbd1771b.

--- Backend/DAL/Entities/Empleado.cs
+++ Backend/DAL/Entities/Empleado.cs
@@ -1,17 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend.DAL.Entities
 {
     public class Empleado : Usuario
     {
         [Required] //FK
-        [ForeignKey("Usuario")]
         public long IdUsuario { get; set; }
         public float Sueldo { get; set; }
         public string TipoEmpleado { get; set; }
         public string HoraInicio { get; set; }
         public string HoraFin { get; set; }
-        public Usuario Usuario { get; set; }    // Relación uno a uno (propiedad de navegación)
+        public ICollection<Usuario> Usuario { get; set; } //FK
     }
 }
