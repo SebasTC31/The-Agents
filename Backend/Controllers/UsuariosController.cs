d6d087281b3e5e4aab9507c71db16ff760fe5267
DIEGO ALEXANDER VALENCIA CALDERÓN <diegovalencia313667@correo.itm.edu.co>
2024-05-04 18:19:15 -0500

    Métodos nuevos agregados en UsuariosController.cs

--- Backend/Controllers/UsuariosController.cs
+++ Backend/Controllers/UsuariosController.cs
@@ -27,5 +27,64 @@ namespace Backend.Controllers
             return usuarios;
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUsuario(long id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null) return NotFound();
+
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostUsuario(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsuario", new { id = usuario.Id }, usuario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuario(long id, Usuario usuario)
+        {
+            if (id != usuario.Id) return BadRequest();
+
+            _context.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id)) return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsuario(long id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+                return NotFound();
+
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UsuarioExists(long id)
+        {
+            return _context.Usuarios.Any(e => e.Id == id);
+        }
+
     }
 }
+// Si este comentario aparece, significa que si se está enviando los cambios al hacer push.
