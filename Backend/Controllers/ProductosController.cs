7c220e4e59fdf2a577ca6303617ef37678668497
DIEGO ALEXANDER VALENCIA CALDERÓN <diegovalencia313667@correo.itm.edu.co>
2024-05-13 12:41:04 -0500

    ProductosController.cs se agrega

---
+++ Backend/Controllers/ProductosController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend.DAL;
+using Backend.DAL.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly DataBaseContext _context;
+
+        public ProductosController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> GetProductos()
+        {
+            var productos = await _context.Productos.ToListAsync();
+
+            if (productos == null || !productos.Any())
+            {
+                return NotFound();
+            }
+
+            return productos;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Producto>> GetProductoById(long id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return producto;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Producto>> PostProducto(Producto producto)
+        {
+            _context.Productos.Add(producto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductoById", new { id = producto.Id }, producto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProducto(long id, Producto producto)
+        {
+            if (id != producto.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(producto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProducto(long id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Productos.Remove(producto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductoExists(long id)
+        {
+            return _context.Productos.Any(e => e.Id == id);
+        }
+    }
+}
+
